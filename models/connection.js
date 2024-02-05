3e197d241294f4cb72a152ddb06c57eed3ce743c
Zach Noche <106810417+SakuZN@users.noreply.github.com>
2024-02-05 15:43:03 +0800

    add mongodb atlas server connection string

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 // This file is initializing the mongodb connection
 // and exporting it for use in all other files through the module.exports
 const mongoose = require('mongoose');
-const databaseURL = 'mongodb://localhost:27017/logindb';
+const databaseURL = "mongodb+srv://zachnoche:ccapdev1234@ccapdev-test.bsiqgjl.mongodb.net/?retryWrites=true&w=majority";
 
 const options = { useNewUrlParser: true,
   useUnifiedTopology: true,
